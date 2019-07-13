b2112f028b1ec83f8fd209dc6dfd374ab96fb52f
robsalzberg <robsalzberg@gmail.com>
2019-07-13 13:13:54 -0400

    Added class Graph to the ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -13,6 +13,23 @@ class Stack():
 
     def size(self):
         return len(self.stack)
-        
+
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, value):
+        self.vertices[value] = set()
+
+    def isvertex(self, value):
+        return value in self.vertices
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            return
+
+        raise IndexError("Vertices not found")
+
 def earliest_ancestor(ancestors, starting_node):
     pass
