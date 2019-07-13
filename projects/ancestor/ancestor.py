ccb3ec1eefe489f033b94e8185e59feb8cd8a06e
robsalzberg <robsalzberg@gmail.com>
2019-07-13 12:44:00 -0400

    Added Class Stack

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,18 @@
+class Stack():
+    def __init__(self):
+        self.stack = []
 
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+
+        raise IndexError("Stack empty cannot pop")
+
+    def size(self):
+        return len(self.stack)
+        
 def earliest_ancestor(ancestors, starting_node):
     pass
