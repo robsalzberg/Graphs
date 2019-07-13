437067aa289101a6fcf943e32869ab13ca262186
robsalzberg <robsalzberg@gmail.com>
2019-07-13 14:01:37 -0400

    Implement DFS method to ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -30,6 +30,53 @@ class Graph():
             return
 
         raise IndexError("Vertices not found")
+        
+def dfs(self, starting_vertex):
+        # Create empty set
+        vertices = set()
+        # Create empty Stack
+        stack = Stack()
+
+        # Add vertex path to Stack
+        initial_path = [starting_vertex]
+        stack.push(initial_path)
+
+        final_paths = []
+
+        while stack.size() > 0:
+            path = stack.pop()
+            vertex = path[-1]
+
+            if vertex not in vertices:
+                vertices.add(vertex)
+
+                # Available path at the end of traversal
+                # shall be stored in output.
+                if len(self.vertices[vertex]) == 0:
+                    final_paths.append(path)
+
+                for next_vertex in self.vertices[vertex]:
+                    if next_vertex not in vertices:
+                        new_path = path.copy()
+                        new_path.append(next_vertex)
+
+                        stack.push(new_path)
+
+        max_path_len = float('-inf')
+        parent_vertex = float('inf')
+
+        for path in final_paths:
+            if len(path) > max_path_len:
+                max_path_len = len(path)
+                parent_vertex = path[-1]
+
+            if len(path) == max_path_len:
+                parent_vertex = min(parent_vertex, path[-1])
+
+        if parent_vertex == starting_vertex:
+            parent_vertex = -1
+
+        return parent_vertex
 
 def earliest_ancestor(ancestors, starting_node):
     pass
